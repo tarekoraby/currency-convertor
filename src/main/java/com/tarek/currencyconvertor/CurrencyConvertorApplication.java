ee4fbd9e210b8fa56aedd27809744219e68eb98d
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-10-15 12:36:54 +0300

    Rename classes to use camel case

--- src/main/java/com/tarek/currencyconvertor/CurrencyConvertorApplication.java
+++ src/main/java/com/tarek/currencyconvertor/CurrencyConvertorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurrencyconvertorApplication {
+public class CurrencyConvertorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyconvertorApplication.class, args);
+		SpringApplication.run(CurrencyConvertorApplication.class, args);
 	}
 
 }
