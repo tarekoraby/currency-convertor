2264bb1f1c04b499dacdd809300c42860842a91b
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-10-15 12:29:04 +0300

    Initial commit

---
+++ src/main/java/com/tarek/currencyconvertor/CurrencyconvertorApplication.java
@@ -0,0 +1,13 @@
+package com.tarek.currencyconvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyconvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyconvertorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarek/currencyconvertor/CurrencyconvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarek.currencyconvertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyconvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
